7ecbc9022e2d26f8dd3fc64130ec4e77587d051e
BenjiBergfors <benjamin.bergfors@elev.ga.ntig.se>
2019-10-21 08:32:05 +0200

    FightSim

---
+++ Prov Klasser och Instanser/Prov Klasser och Instanser/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prov_Klasser_och_Instanser
+{
+    class Book
+    {
+        public string name;
+
+        public int price; 
+
+        public int rarity;
+
+        public string category;
+
+        public int actualValue;
+
+        public bool cursed;
+
+        public Random generator;
+
+        internal class price : Random
+        {
+        }
+
+        //public void PrintInfo;
+
+
+    }
+    
+    
+
+
+
+    
+    
+
+}
---
+++ Prov Klasser och Instanser/Prov Klasser och Instanser/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prov_Klasser_och_Instanser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           Book book1 = new Book();
+            {
+                book1.name = "Kakornas Historia";
+                book1.category = "History";
+                book1.cursed = false;
+
+                
+            
+
+                Console.WriteLine("Name: " + book1.name + "Category: " + book1.category);
+                Console.ReadLine();
+            }
+
+
+
+
+        }
+    }
+}
